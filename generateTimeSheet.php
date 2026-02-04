e2a71a8ea602eebb447232ebcb696276b839e815
Lance Mariquit <lancemariquit.1214@gmail.com>
2026-02-04 11:03:25 +0800

    Fixed typos

--- generateTimeSheet.php
+++ generateTimeSheet.php
@@ -6,7 +6,7 @@
     $intern_display_id = $_SESSION['intern_display_id'];
     $templatePath = 'assets/spreadsheets/template/TIME_SHEET_SUMMARY.xlsx';
     $targetDir = 'assets/spreadsheets/' . $intern_display_id . '/';
-    $newFileName =  $intern_display_id . 'TIME_SHEET_SUMMARY_.xlsx';
+    $newFileName =  $intern_display_id . '-TIME_SHEET_SUMMARY_.xlsx';
     $destinationPath = $targetDir . $newFileName;
 
     $_SESSION['time_sheet_template'] = $templatePath;
