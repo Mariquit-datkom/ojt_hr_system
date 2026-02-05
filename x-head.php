250bf62febee5512100ab94242695e45e52d3315
Lance Mariquit <lancemariquit.1214@gmail.com>
2026-02-05 15:09:21 +0800

    Added misc files

--- x-head.php
+++ x-head.php
@@ -2,5 +2,5 @@
     <!-- Bootstrap CSS CDN -->
   
     <!-- Font Awesome CDN -->
-    <link rel="stylesheet" href="assets/fontawesome-free-7.0.0-web/css/all.min.css">
+    <link rel="stylesheet" href="libs/fontawesome-free-7.0.0-web/css/all.min.css">
  
