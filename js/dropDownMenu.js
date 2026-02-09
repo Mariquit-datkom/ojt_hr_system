e2159a85d1159b08e5cc75593563e48fb408bd7b
Lance Mariquit <lancemariquit.1214@gmail.com>
2026-02-09 14:53:53 +0800

    Fixed minor bugs

--- js/dropDownMenu.js
+++ js/dropDownMenu.js
@@ -1,16 +1,18 @@
-//User icon drop down menu for log out
 const avatarBtn = document.getElementById('user-avatar-btn');
 const dropdown = document.getElementById('user-avatar-dropdown');
 
-avatarBtn.onclick = function(event) {
+// Toggle the dropdown when clicking the avatar
+avatarBtn.addEventListener('click', function(event) {
     dropdown.classList.toggle('show');
     event.stopPropagation();
-}
+});
 
-window.onclick = function(event) {
-    if (!event.target.matches('.user-avatar')) {
+// Close the dropdown when clicking anywhere else
+window.addEventListener('click', function(event) {
+    // Check if the click was OUTSIDE the button and OUTSIDE the dropdown
+    if (!avatarBtn.contains(event.target) && !dropdown.contains(event.target)) {
         if (dropdown.classList.contains('show')) {
             dropdown.classList.remove('show');
         }
     }
-}
+});
